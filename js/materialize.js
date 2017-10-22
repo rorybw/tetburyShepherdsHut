4d83c75202aaab1b3d7dbdf3f0a37d409a8dc190
Rory B-W <rory.batewilliams@gmail.com>
2017-10-22 21:39:37 +0100

    Update

--- js/materialize.js
+++ js/materialize.js
@@ -60,9 +60,13 @@ $(document).ready(function(){
       });
     });
 
+$(document).ready(function() {
+    $('select').material_select();
+});
+
 $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+    $('.parallax').parallax();
+});
 
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
